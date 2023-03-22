72ac9a24aefec2169aa59f40e0eacc1cab7bd992
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-03-22 14:09:46 +0900

    [Bronze V] Title: A/B, Time: 208 ms, Memory: 17780 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        double a = in.nextInt();
+        double b = in.nextInt();
+        double c= a/b;
+        System.out.print(c);
+    }
+}
