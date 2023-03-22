4475181e51ec950f89993a95f55cf283373749a4
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-03-22 17:00:45 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 820 ms, Memory: 298556 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -9,12 +9,12 @@ public class Main {
         int N=in.nextInt(); //N명
         int K=in.nextInt(); //K번째 사람 제거
         for(int i=1;i<=N;i++){
-            list.add(i); //큐에 1~N 저장
+            list.add(i); //리스트에 1~N 저장
         }
         System.out.print("<");
         while(!list.isEmpty()){ //연결리스트가 빌 떄까지 반복
             for(int j=1;j<=K;j++){
-                if(j==K){
+                if(j==K){ //K번째인 경우 
                     int a = list.get(0); //K번 째 리스트 값 저장
                     list.remove(0); //연결리스트에서 값 삭제
                     if(list.size()==0){ //연결리스트 크기가 0인 경우
@@ -24,10 +24,10 @@ public class Main {
                         System.out.print(a+", ");
                     }
                 }
-                else{
-                    int b = list.get(0);
-                    list.remove(0);
-                    list.add(b);
+                else{ //K번째가 아닌 경우
+                    int b = list.get(0); //리스트 값 변수에 저장 후
+                    list.remove(0); //제거
+                    list.add(b); //다시 추가
                 }
             }
         }
