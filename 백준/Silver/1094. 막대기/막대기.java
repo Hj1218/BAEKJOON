551f93c7c52731f9d4f2dbe43566140536c6d5de
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-04-06 22:50:14 +0900

    [Silver V] Title: 막대기, Time: 212 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int a = 64;
+        int count=0;
+        while(true){
+            if(x>a){
+                x=x-a;
+                count++;
+            }
+            else if(x<a){
+                a=a/2;
+            }
+            else{
+                count++;
+                break;
+            }
+        }
+        System.out.print(count);
+    }
+}
