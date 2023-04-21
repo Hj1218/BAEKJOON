9c6479b5e52697a933fbce9b1cf42f2a087cea46
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-04-21 22:10:56 +0900

    [Bronze V] Title: 아!, Time: 204 ms, Memory: 17572 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+
+        if(a.length()<b.length()){
+            System.out.print("no");
+        }
+        else{
+            System.out.print("go");
+        }
+    }
+}
