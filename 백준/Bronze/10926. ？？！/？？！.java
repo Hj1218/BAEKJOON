08841b845cc027d139412b90295615214bc1d217
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-04-24 21:48:27 +0900

    [Bronze V] Title: ??!, Time: 224 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
+        String s = in.next();
+        sb.append(s).append("??!");
+        System.out.print(sb);
+    }
+}
