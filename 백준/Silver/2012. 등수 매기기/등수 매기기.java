6f09b10646bb3267cdf7680e0cbaae803199a2e0
정현지 <114800998+Hj1218@users.noreply.github.com>
2023-05-16 23:23:29 +0900

    [Silver III] Title: 등수 매기기, Time: 1980 ms, Memory: 310320 KB -BaekjoonHub

---
+++ 백준/Silver/2012. 등수 매기기/등수 매기기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int [] num = new int[n];
+        for(int i=0;i<n;i++){
+            num[i]=in.nextInt();
+        }
+        Arrays.sort(num);
+        long count =0;
+        for(int j=0;j<n;j++){
+            count+=Math.abs(num[j]-j-1);
+        }
+        System.out.print(count);
+    }
+}
